89c39041b9d40bbac555532586f2dd135b700c29
Danila <47973467+katokdoescode@users.noreply.github.com>
2025-06-18 16:44:10 +0400

    Revert "Init shadcn config"

--- components/AuthDialog.vue
+++ components/AuthDialog.vue
@@ -1,110 +1,56 @@
 <template>
-  <UDialog>
-    <UDialogTrigger as-child>
-      <UButton>
-        {{ mode === 'signIn' ? 'Sign In' : 'Sign Up' }}
-      </UButton>
-    </UDialogTrigger>
-    <UDialogContent class="sm:max-w-[425px]">
-      <UTabs
-        v-model="mode"
-        class="mt-4"
-      >
-        <UTabsList class="w-full">
-          <UTabsTrigger value="signIn">
-            Sign In
-          </UTabsTrigger>
-          <UTabsTrigger value="signUp">
-            Sign Up
-          </UTabsTrigger>
-        </UTabsList>
-      </UTabs>
-      <UDialogHeader>
-        <UDialogTitle>{{ mode === 'signIn' ? 'Sign In' : 'Sign Up' }}</UDialogTitle>
-        <UDialogDescription>
-          {{ mode === 'signIn' ? 'Sign in to your account' : 'Create a new account' }}
-        </UDialogDescription>
-      </UDialogHeader>
-      <div class="flex flex-col gap-4 py-4">
-        <div class="space-y-2">
-          <ULabel for="email">
-            Email
-          </ULabel>
-          <UInput
-            id="email"
-            v-model="email"
-            placeholder="Email"
-            type="email"
-            class="w-full"
-          />
-        </div>
-        <div class="space-y-2">
-          <ULabel for="password">
-            Password
-          </ULabel>
-          <UInput
-            id="password"
-            v-model="password"
-            placeholder="Password"
-            type="password"
-            class="w-full"
-          />
-        </div>
-        <UButton
-          :disabled="loading"
-          class="w-full"
-          @click="handleAuth"
-        >
-          <span
-            v-if="loading"
-            class="mr-2 h-4 w-4 animate-spin rounded-full border-2 border-current border-t-transparent"
-          />
+  <UModal :title="mode === 'signIn' ? 'Sign In' : 'Sign Up'" :open="open" @update:open="onDialogUpdate"
+    aria-describedby="auth-dialog-description">
+    <template #description>
+      <p id="auth-dialog-description">
+        {{ mode === 'signIn' ? 'Sign in to your account' : 'Create a new account' }}
+      </p>
+    </template>
+    <template #body>
+      <div class="flex flex-col gap-4">
+        <UInput v-model="email" placeholder="Email" type="email" class="w-full" />
+        <UInput v-model="password" placeholder="Password" type="password" class="w-full" />
+        <UButton :loading="loading" class="mt-2 self-start" @click="handleAuth">
           {{ mode === 'signIn' ? 'Sign In' : 'Sign Up' }}
         </UButton>
-        <div class="text-center text-sm">
+        <div class="text-center text-xs mt-2">
           <span v-if="mode === 'signIn'">
             Don't have an account?
-            <UButton
-              variant="link"
-              size="sm"
-              class="p-0 h-auto"
-              @click="mode = 'signUp'"
-            >Sign Up</UButton>
+            <UButton variant="link" color="primary" size="xs" @click="mode = 'signUp'">Sign Up</UButton>
           </span>
           <span v-else>
             Already have an account?
-            <UButton
-              variant="link"
-              size="sm"
-              class="p-0 h-auto"
-              @click="mode = 'signIn'"
-            >Sign In</UButton>
+            <UButton variant="link" color="primary" size="xs" @click="mode = 'signIn'">Sign In</UButton>
           </span>
         </div>
-        <div
-          v-if="error"
-          class="text-sm text-destructive text-center"
-        >
+        <div v-if="error" class="text-red-500 text-xs text-center mt-2">
           {{ error }}
         </div>
       </div>
-    </UDialogContent>
-  </UDialog>
+    </template>
+  </UModal>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 import { useSupabaseClient } from '#imports';
+import { useAuthDialog } from '~/composables/useAuthDialog';
+
+const { open, mode, setOpen } = useAuthDialog();
 
 const email = ref('');
 const password = ref('');
 const loading = ref(false);
 const error = ref('');
-const mode = ref<'signIn' | 'signUp'>('signIn');
 
 const supabase = useSupabaseClient();
 
+function onDialogUpdate(val: boolean) {
+  setOpen(val);
+}
+
 function close() {
+  setOpen(false);
   error.value = '';
   email.value = '';
   password.value = '';
